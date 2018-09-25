b54e6e2b7a537cc103aae0389a4b5931cfe26c45
dell951 <dell951.dell@gmail.com>
2018-09-25 22:42:08 +0800

    ignore more pycs

--- vtbscrapy/runscrapy.sh
+++ vtbscrapy/runscrapy.sh
@@ -3,7 +3,7 @@
 mkdir -p alldone
 
 sudo docker run --rm -v $(pwd):/runtime/app myscrapydocker scrapy crawl vtb -a studio=$1 -a title=$2
-mv -n alldone/* /volumeUSB1/usbshare1-2/porn/pretty/
+mv alldone/* /volumeUSB1/usbshare1-2/porn/pretty/
 
 #scrapy crawl vtb -a studio=$1 -a title=$2
 #sshpass -f /Users/atext scp -rp alldone/. admin@192.168.1.150:/volumeUSB1/usbshare1-2/porn/pretty
