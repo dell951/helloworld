01a8c3026dbf7e57bebef9af346e88d8c311f6ed
dell951 <dell951.dell@gmail.com>
2018-09-25 22:21:15 +0800

    clean up

--- vtbscrapy/docker-runscrapy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p alldone
-
-sudo docker run --rm -v $(pwd):/runtime/app myscrapydocker scrapy crawl vtb -a studio=$1 -a title=$2
-mv -n alldone/* /volumeUSB1/usbshare1-2/porn/pretty/
-
-rm -rf alldone
--- vtbscrapy/runscrapy.sh
+++ vtbscrapy/runscrapy.sh
@@ -2,7 +2,10 @@
 
 mkdir -p alldone
 
-scrapy crawl vtb -a studio=$1 -a title=$2
-sshpass -f /Users/atext scp -rp alldone/. admin@192.168.1.150:/volumeUSB1/usbshare1-2/porn/pretty
+sudo docker run --rm -v $(pwd):/runtime/app myscrapydocker scrapy crawl vtb -a studio=$1 -a title=$2
+mv -n alldone/* /volumeUSB1/usbshare1-2/porn/pretty/
+
+#scrapy crawl vtb -a studio=$1 -a title=$2
+#sshpass -f /Users/atext scp -rp alldone/. admin@192.168.1.150:/volumeUSB1/usbshare1-2/porn/pretty
 
 rm -rf alldone
