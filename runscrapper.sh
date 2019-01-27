644bf2f3bf69476ea2e7d201642746cdb4b2762b
dell951 <dell951.dell@gmail.com>
2019-01-27 13:58:01 +0800

    reverted to dmm

--- runscrapper.sh
+++ runscrapper.sh
@@ -6,7 +6,7 @@ export JAVA_HOME=/var/packages/Java8/target/j2sdk-image
 cd /volume1/nas-share/helloworld
 git checkout -- settings.xml
 if [ ! -n "$2" ] ;then
-    python /volume1/nas-share/helloworld/scrapy_wrapper.py $1 javbus
+    python /volume1/nas-share/helloworld/scrapy_wrapper.py $1 dmm
 else
     python /volume1/nas-share/helloworld/scrapy_wrapper.py $1 $2
 fi    
