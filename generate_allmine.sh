c16b0bec2c346ab11765242bdaf9d0cd1d54bcdc
dell951 <dell951.dell@gmail.com>
2019-02-15 23:27:08 +0800

    adding 8tb

--- generate_allmine.sh
+++ generate_allmine.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 rm -rf /volume1/nas-share/helloworld/allmine.txt
-find /volumeUSB1/usbshare1-2/porn/ /volume3/czimu/ /volume1/3TB/czimu/ /volume1/3TB/iReady2/ /volume3/iReady2/ /volume2/formated/ -type f \( -iname \*.mp4 -o -iname \*.avi -o -iname \*.mkv -o -iname \*.rmvb -o -iname \*.wmv \) |sort -u >> /volume1/nas-share/helloworld/allmine.txt
+find /volumeUSB1/usbshare1-2/porn/ /volume3/czimu/ /volume4/8TB/czimu/ /volume4/8TB/iReady2/ /volume1/3TB/czimu/ /volume1/3TB/iReady2/ /volume3/iReady2/ /volume2/formated/ -type f \( -iname \*.mp4 -o -iname \*.avi -o -iname \*.mkv -o -iname \*.rmvb -o -iname \*.wmv \) |sort -u >> /volume1/nas-share/helloworld/allmine.txt
+
+echo "all done." >> /volume1/nas-share/helloworld/allmine.txt
