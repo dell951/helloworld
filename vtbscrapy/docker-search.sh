7c0c1789cc7a0f5deb1bab4c3adbffdd5a0e3c70
dell951 <dell951.dell@gmail.com>
2019-03-17 17:09:03 +0800

    minor updates

--- vtbscrapy/docker-search.sh
+++ vtbscrapy/docker-search.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo docker run --rm -v $(pwd):/runtime/app myscrapydocker:1.2 scrapy crawl search -a studio=$1 -a queryKey="$2" -a filedate="$3"
+
+sudo docker run --rm -v $(pwd):/runtime/app myscrapydocker:1.2 scrapy crawl search -a studio=$1 -a queryKey="$2" -a filedate="$3" -a showFullresult=$4
