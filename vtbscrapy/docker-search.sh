d3115e68e2f49c19b41204b6757b8ed4b1027ce0
dell951 <dell951.dell@gmail.com>
2019-03-24 00:22:56 +0800

    support new search

--- vtbscrapy/docker-search.sh
+++ vtbscrapy/docker-search.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 
-sudo docker run --rm -v $(pwd):/runtime/app myscrapydocker:1.2 scrapy crawl search -a studio=$1 -a queryKey="$2" -a filedate="$3" -a showFullresult=$4
+CRAWLER="vtb"
+
+if [ $1 == "letsdoeit" ]
+then
+   CRAWLER="searchletsdoeit"
+fi
+
+#echo $CRAWLER
+
+sudo docker run --rm -v $(pwd):/runtime/app myscrapydocker:1.2 scrapy crawl $CRAWLER -a studio=$1 -a queryKey="$2" -a filedate="$3" -a showFullresult=$4
