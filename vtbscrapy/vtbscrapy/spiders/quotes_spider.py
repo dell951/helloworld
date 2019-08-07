b88eba46a1476870afbfef9be6f002308ad3310e
dell951 <dell951.dell@gmail.com>
2019-08-08 01:07:19 +0800

    reverted from pyc...

--- vtbscrapy/vtbscrapy/spiders/quotes_spider.py
+++ vtbscrapy/vtbscrapy/spiders/quotes_spider.py
@@ -1,16 +1,11 @@
-#!/usr/bin/python
-#coding:utf-8
-
 import scrapy
-import os
-import subprocess
-import urlparse
-import logging
 import requests
 import shutil
+import subprocess
+import os
+import codecs
 import re
-from scrapy_splash import SplashRequest
-from scrapy.selector import HtmlXPathSelector
+import json
 
 nfoTemplate = """<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
 <movie>
@@ -34,7 +29,7 @@ nfoTemplate = """<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
     <thumb>%(fanart)s</thumb>
   </fanart>
   <mpaa></mpaa>
-  <id>%(mid)s</id>
+  <id>%(title)s</id>
   <genre></genre>
   %(actors)s
   <director></director>
@@ -47,98 +42,128 @@ actorTemplate = """  <actor>
   </actor>
 """
 
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'
-movie_XPath = "//div[contains(@class,'Container')]//div[contains(@class,'Column')]//section"
+old_title_XPath = "//div[contains(@class,'player-caption')]//h1[contains(@class,'caption-title')]/a/text()"
+old_stars_XPath = "//div[contains(@class,'player-caption')]//p//a/text()"
+old_startsURL_XPath = "//div[contains(@class,'player-caption')]//p//a/@href"
+old_rate_XPath = "//div[contains(@class,'player-caption')]//span[contains(@class,'totalrate')]/text()"
+old_date_XPath = "//div[contains(@class,'player-description')]//span[contains(@class, 'info')]/text()"
+old_desc_XPath = "//div[contains(@class,'player-description')]//span[contains(@class, 'moreless')]/text()"
+old_fanart_XPath = "//div[contains(@class,'swiper-wrapper')]//a[contains(@class,'swiper-content-item')]/img/@src"
+old_poster_XPath = "//div[contains(@class, 'player-img-wrap')]//img/@src"
 
-title_XPath = "//h3[contains(@class,'h1')]/text()"
-poster_url_XPath = ""
-fanart_url_XPath = "//div[contains(@class,'content-cover')]/img/@src"
-desc_XPath = "//div[contains(@class,'blog-single')]/p/text()"
-actros_XPath = "//div[contains(@class,'about-author')]//h5/text()"
-date_XPath = "//div[contains(@class,'single-meta')]//span[contains(@class,'date')]/text()" #the date is the 5th
+class QuotesSpider(scrapy.Spider):
+    name = 'vtb'
+    studio = ''
+    title = ''
+    actors = ''
+    movie_title = ''
+    movie_rate = ''
+    movie_desc = ''
+    movie_date = ''
+    poster_url = ''
+    fanart_url = ''
+    
+    def __init__(self, studio, title, *args, **kwargs):
+        super(QuotesSpider, self).__init__(*args, **kwargs)
+        self.studio = studio
+        self.title = title
 
-logging.getLogger().setLevel(logging.INFO)
-
-class BabesSpider(scrapy.Spider):
-    name = "babes"    
-    fid = ""
-    movie_id = ""
-    studio = "Babes"
-    actors = ""
-    movie_title = ""
-    movie_rate = ""
-    movie_desc = ""
-    movie_date = ""
-    poster_url = ""
-    fanart_url = ""
-    movie_star_photo = ""
-
-    def __init__(self, fid, *args, **kwargs):
-        self.fid = fid
-        self.movie_id = fid.split('/')[3]
-        super(BabesSpider, self).__init__(*args, **kwargs)
-        self.baseUrl = 'https://www.' + self.studio + '.com'+ self.fid
-        print "proceed %s" % self.baseUrl
-
+    
     def start_requests(self):
         urls = [
-            self.baseUrl
-        ]
-
-        script = """
-        function main(splash)
-            assert(splash:go(splash.args.url))
-            assert(splash:wait(5.5))
-            return {html=splash:html()}
-        end
-        """
-
+            'https://www.' + self.studio + '.com/' + self.title]
         for url in urls:
-            print url
-            yield SplashRequest(url=url, callback=self.parse, endpoint='execute', args={'wait': 5.5, 'lua_source':script})
+            print 'Proceeding %s' % url
+            request = scrapy.Request(url = url, callback = self.parse_byJson)
+            request.meta['dont_redirect'] = True
+            yield request
+            None
+    
+    def parse_byJson(self, response):
+        json_text = re.search('window.__INITIAL_STATE__ = (.*)?;', response.text)
+        if json_text:
+            json_obj = json.loads(json_text.group(1))
+            allvideos = json_obj['videos']
+            movie_data = allvideos[len(allvideos) - 1]
+            page_data = json_obj['page']['data']['/%s' % self.title]['data']
+            self.movie_title = movie_data['title']
+            print 'Movie Tile         - %s' % self.movie_title.encode('utf-8')
+            self.movie_rate = movie_data['rating']
+            print 'Rate               - %s' % self.movie_rate
+            self.movie_date = movie_data['releaseDateFormatted']
+            print 'Release Date       - %s' % self.movie_date
+            self.movie_desc = movie_data['description']
+            print 'Description        - %s' % self.movie_desc.encode('utf-8')
+            posters = movie_data['images']['poster']
+            subprocess.call('mkdir -p alldone', shell = True)
+            for poster in posters:
+                if '1920x1080' in poster['name']:
+                    self.poster_url = poster['src']
+                    print 'poster             - %s' % self.poster_url
+                    rposter = requests.get(self.poster_url, stream = True)
+                    poster_path = self.title + '-poster.jpg'
+                    if rposter.status_code == 200:
+                        with open(os.path.join('alldone', poster_path), 'wb') as f0:
+                            rposter.raw.decode_content = True
+                            shutil.copyfileobj(rposter.raw, f0)
+                    
+            fanart = page_data['pictureset'][0]
+            self.fanart_url = fanart['main'][0]['src']
+            print 'fanart             - %s' % self.fanart_url
+            rfanart = requests.get(self.fanart_url, stream = True)
+            fanart_path = self.title + '-fanart.jpg'
+            if rfanart.status_code == 200:
+                with open(os.path.join('alldone', fanart_path), 'wb') as f1:
+                    rfanart.raw.decode_content = True
+                    shutil.copyfileobj(rfanart.raw, f1)
+            models = movie_data['modelsSlugged']
+            for model in models:
+                star_url = '/%s' % model['slugged']
+                print 'star_url             - %s' % star_url
+                star_page = response.urljoin(star_url)
+                yield scrapy.Request(star_page, callback = self.parse_stars_by_json)
+                None
+            
+        else:
+            print "Somehow I didn't find the Json data[Movie]"
 
-    def parse(self, response):
-        #print response.text
-        hxs = HtmlXPathSelector(response)
-        movie_item = hxs.select(movie_XPath)[0]
-        self.movie_title = movie_item.select('.//h2/text()').extract_first().strip()
-        print "Title --> %s" % self.movie_title.encode('utf-8')
-        self.poster_url = movie_item.select('.//img/@src').extract_first()
-        self.movie_star_photo = self.poster_url
-        print "Poster url ---> %s" % self.poster_url
-        #headers = {'User-Agent': user_agent}
-        #rposter = requests.get(self.poster_url, headers=headers)
-        poster_path = 'alldone/' + self.movie_id + "-poster.jpg"
-        #if rposter.status_code == 200:
-        #    with open(os.path.join('alldone/',poster_path), 'wb') as f0:
-        #        rposter.raw.decode_content = True
-        #        shutil.copyfileobj(rposter.raw, f0) 
-        cmd = 'curl %s -o %s' % (self.poster_url, poster_path)
-        print cmd
-        subprocess.call([cmd], shell=True)
-        print "poster saved."
-        #self.fanart_url = response.xpath(fanart_url_XPath).extract_first()
-        #print "Fanart url --> %s" % self.fanart_url
-        #rfanart = requests.get(self.fanart_url, stream=True)
-        #fanart_path = self.fid + "-fanart.jpg"
-        #if rfanart.status_code == 200:
-        #    with open(os.path.join('alldone/',fanart_path), 'wb') as f0:
-        #        rfanart.raw.decode_content = True
-        #        shutil.copyfileobj(rfanart.raw, f0) 
-        #self.movie_desc = response.xpath(desc_XPath).extract_first()
-        #print "Desc ---> %s" % self.movie_desc.encode('utf-8')
+    
+    def parse_stars_by_json(self, response):
+        json_text = re.search('window.__INITIAL_STATE__ = (.*)?;', response.text)
+        if json_text:
+            json_obj = json.loads(json_text.group(1))
+            star_data = json_obj['models'][0]
+            star_name = star_data['description']
+            star_photo_url = star_data['images']['profile'][0]['src']
+            subprocess.call('mkdir -p alldone/.actors', shell = True)
+            print 'star actor         - %s' % star_photo_url
+            rstar_photo = requests.get(star_photo_url, stream = True)
+            star_photo_path = star_name.replace(' ', '_') + '.jpg'
+            if rstar_photo.status_code == 200:
+                with open(os.path.join('alldone/.actors', star_photo_path), 'wb') as f2:
+                    rstar_photo.raw.decode_content = True
+                    shutil.copyfileobj(rstar_photo.raw, f2)
+            self.actors = self.actors + actorTemplate % {
+                'movie_star': star_name,
+                'movie_star_photo': star_photo_url }
+        else:
+            print "Somehow I didn't find the Json data [Star]"
 
-        actor = movie_item.select('/div/div/div/span/a/text()')      
-        self.actors = actorTemplate%{'movie_star': actor, 'movie_star_photo': self.movie_star_photo}
-        print "Actor --> %s" %self.actors.encode('utf-8')
-
-        print "Date --> %s" % self.movie_date
-       
-        nfoInfo = nfoTemplate%{'movie_title': self.movie_title,'movie_desc': self.movie_desc, 'movie_rate': "",
-            'movie_date': self.movie_date, 'mid': self.movie_id, 'actors': self.actors, 'studio': "", 'poster': self.poster_url, 'fanart': self.fanart_url}
-        with open(os.path.join('alldone/', self.fid +".nfo"), "w") as nfofile:
-            nfofile.write(nfoInfo.encode('utf-8'))
-        print "%s NFO/Poster/Fanart Saved." % self.fid
-
+    
     def closed(self, reason):
-        pass
+        if self.movie_title != '':
+            nfoInfo = nfoTemplate % {
+                'movie_title': self.movie_title,
+                'movie_desc': self.movie_desc,
+                'movie_rate': self.movie_rate,
+                'movie_date': self.movie_date,
+                'title': self.title,
+                'actors': self.actors,
+                'studio': self.studio,
+                'poster': self.poster_url,
+                'fanart': self.fanart_url }
+            with open(os.path.join('alldone/', self.title + '.nfo'), 'w') as nfofile:
+                nfofile.write(nfoInfo.encode('utf-8'))
+            print '%s NFO Saved.' % self.title.encode('utf-8')
+        else:
+            print 'Do nothing.'
