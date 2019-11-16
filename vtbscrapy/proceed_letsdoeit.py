ccaa1a18bd8c6a439e85737720d63ea839dee5ad
dell951 <dell951.dell@gmail.com>
2019-11-16 13:51:34 +0800

    fix letsdoeit filename output

--- vtbscrapy/proceed_letsdoeit.py
+++ vtbscrapy/proceed_letsdoeit.py
@@ -45,7 +45,7 @@ for parent, dirnames, filenames in os.walk(search_path):
                             print targetFileName
                             matchId = re.search(r'https://letsdoeit.com/watch/(\d*?)/(.*?)\.en\.html',targetFileName)
                             if matchId:
-                                print "mv %s %s.mp4" % (full_path, os.path.join(parent,matchId.group[2]))
+                                print "mv %s %s.mp4" % (full_path, os.path.join(parent,matchId.group(2)))
                             else:
                                 print "not found matchId for %s" % targetFileName
 
