ec3fed1583a2406d561800d501d05dbb1e9a3ee8
dell951 <dell951.dell@gmail.com>
2020-09-05 15:11:43 +0800

    generate updating...

--- backup_full.sh
+++ backup_full.sh
@@ -4,10 +4,12 @@ now="$(date +'%Y%m%d')"
 echo $now
 
 backup_nfo.py /volume1/3TB/czimu/ $now/czimu
+backup_nfo.py /volume1/3TB/hczimu/ $now/czimu
 backup_nfo.py /volume1/3TB/iReady2/ $now/iReady
 backup_nfo.py /volume3/czimu/ $now/czimu
 backup_nfo.py /volume3/iReady2/ $now/iReady
 backup_nfo.py /volume4/8TB/czimu/ $now/czimu/
+backup_nfo.py /volume4/8TB/hczimu/ $now/czimu/
 backup_nfo.py /volume4/8TB/iReady2/ $now/iReady
 backup_nfo.py /volume2/14TB/allinone/iReady2/ $now/iReady
 backup_nfo.py /volume2/14TB/allinone/czimu/ $now/czimu
@@ -18,6 +20,7 @@ backup_nfo.py /volumeUSB1/usbshare1-2/porn/plexready/ $now/iReady
 backup_nfo.py /volumeUSB1/usbshare1-2/porn/seriesformated $now/series
 backup_nfo.py /volumeUSB1/usbshare1-2/porn/S-Cute/ $now/scute
 backup_nfo.py /volumeUSB1/usbshare1-2/porn/pretty/ $now/pretty
+backup_nfo.py /volumeUSB1/usbshare1-2/HDsMovie $now/HDsMovie
 backup_nfo.py /volume2/14TB/pretty/ $now/pretty
 
 tar cvzf fullnfo_bk.tgz $now/
--- runscrapper.sh
+++ runscrapper.sh
@@ -6,7 +6,7 @@ export JAVA_HOME=/var/packages/Java8/target/j2sdk-image
 cd /volume1/nas-share/helloworld
 cp settings.xml.bk settings.xml
 if [ ! -n "$2" ] ;then
-    python /volume1/nas-share/helloworld/scrapy_wrapper.py $1 dmm
+    python /volume1/nas-share/helloworld/scrapy_wrapper.py $1 javbus
 else
     python /volume1/nas-share/helloworld/scrapy_wrapper.py $1 $2
 fi    
